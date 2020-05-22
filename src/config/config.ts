5433be503ed07bc317b9911f7820bb747bc5137a
Tidjani <mty.tidjani@gmail.com>
2020-05-22 22:31:14 +0100

    Beffore ffmpeg comes in

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,9 @@ const ev = process.env;
 const config = {
   port: ev.APP_PORT,
   logDir: ev.LOG_DIR,
+  imgPath: ev.IMAGES_PATH,
+  vidPath: ev.VIDEOS_PATH,
+  docPath: ev.DOCS_PATH,
 };
 
 export default config;
