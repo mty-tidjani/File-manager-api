70e903282a06a0d150477798467b538fbce72764
Tidjani <mty.tidjani@gmail.com>
2020-05-23 00:02:30 +0100

    Fixing TypeScript Typology

--- src/middlewares/files.meta.ts
+++ src/middlewares/files.meta.ts
@@ -2,23 +2,22 @@ import fs from 'fs';
 import path from 'path';
 import ffmpegStatic from 'ffmpeg-static';
 import ffprobeStatic from 'ffprobe-static';
-import ffmpeg from 'fluent-ffmpeg';
+import fluentFfmpeg from 'fluent-ffmpeg';
 import { Request, Response, NextFunction } from 'express';
 
 
 const saveThumb = (file: Express.Multer.File) => new Promise((resolve) => {
-  ffmpeg(file.path).takeScreenshots({
+  fluentFfmpeg(file.path).takeScreenshots({
     count: 1, timemarks: ['50%'],
-    filename: `${file.filename}.png` },
-                                    'uploads/videos/thumbs').on('end', () => {
-                                      resolve(`${file.filename}.png`);
-                                    }).on('error', () => {
-                                      resolve('default_thumbnail.png');
-                                    });
+    filename: `${file.filename}.png` },   'uploads/videos/thumbs').on('end', () => {
+      resolve(`${file.filename}.png`);
+    }).on('error', () => {
+      resolve('default_thumbnail.png');
+    });
 });
 
 const getMetadata = (file: Express.Multer.File) => new Promise((resolve) => {
-  ffmpeg.ffprobe(file.path, (err: any, metadata: any) => {
+  fluentFfmpeg.ffprobe(file.path, (err: any, metadata: any) => {
     if (metadata) {
       const meta = {
         ...metadata.format,
@@ -32,22 +31,23 @@ const getMetadata = (file: Express.Multer.File) => new Promise((resolve) => {
   });
 });
 
-export const loadMetadata = (type = 'video') => async (req, res, next) => {
+export const loadMetadata = (type = 'video') => async (req: any, res: Response<any>, next: NextFunction) => {
   const { file } = req;
-  ffmpeg.setFfmpegPath(ffstatic);
-  ffmpeg.setFfprobePath(ffprobe.path);
-  const meta = await getMetadata(file);
+  fluentFfmpeg.setFfmpegPath(ffmpegStatic);
+  fluentFfmpeg.setFfprobePath(ffprobeStatic.path);
+  const meta: any = await getMetadata(file);
   if (type === 'video') meta.thumbnail = await saveThumb(file);
   // console.log(proc);
   req.metadata = meta;
   return next();
 };
 
-export const loadImagesMeta = async (req: Request, res: Response<any>, next: NextFunction) => {
-  ffmpeg.setFfmpegPath(ffstatic);
-  ffmpeg.setFfprobePath(ffprobe.path);
-  for (let i = 0; i < req.files.length; i += 1) {
-    req.files[i].meta = await getMetadata(req.files[i]);
+export const loadImagesMeta = async (req: any, res: Response<any>, next: NextFunction) => {
+  fluentFfmpeg.setFfmpegPath(ffmpegStatic);
+  fluentFfmpeg.setFfprobePath(ffprobeStatic.path);
+
+  for (const file of req.files) {
+    file.meta = await getMetadata(file);
   }
   return next();
 };
