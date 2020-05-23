82485461cb930039d2a5a81d42494596cf04ec0c
Tidjani <mty.tidjani@gmail.com>
2020-05-23 10:09:32 +0100

    Update: made image crop to return a

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,18 @@
 require('dotenv').config();
+import ffmpegStatic from 'ffmpeg-static';
+import ffprobeStatic from 'ffprobe-static';
+import fluentFfmpeg from 'fluent-ffmpeg';
 
 const ev = process.env;
-
+fluentFfmpeg.setFfmpegPath(ffmpegStatic);
+fluentFfmpeg.setFfprobePath(ffprobeStatic.path);
 const config = {
   port: ev.APP_PORT,
   logDir: ev.LOG_DIR,
   imgPath: ev.IMAGES_PATH,
   vidPath: ev.VIDEOS_PATH,
   docPath: ev.DOCS_PATH,
+  ffmpeg: fluentFfmpeg,
 };
 
 export default config;
--- src/controllers/streamController.ts
+++ src/controllers/streamController.ts
@@ -2,6 +2,44 @@ import { Request, Response, NextFunction } from 'express';
 import sharp from 'sharp';
 import path from 'path';
 import fs from 'fs';
+import config from '../config/config';
+
+const getMeta = (filePath:string) => {
+  return new Promise((resolve) => {
+    config.ffmpeg.ffprobe(filePath, (err: any, metadata: any) => {
+      console.log(err);
+      if (metadata) {
+        const meta: any = {};
+        meta.width = metadata.streams[0].width;
+        meta.height = metadata.streams[0].height;
+        return resolve(meta);
+      }
+      resolve(null);
+    });
+  });
+};
+
+const optimize = async (options: any, filePath: string) => {
+  const meta: any = await getMeta(filePath);
+  console.log('meta ------------', meta);
+  if (!meta) return options;
+  if (options.width && options.height) {
+    if (meta.width >= options.width && meta.height >= options.height) return options;
+    const ref = meta.width > meta.height ? meta.height : meta.width;
+    const newW = options.width / (options.width / ref);
+    const newH = options.height / (options.height / ref);
+    return { ...options, width: newW, height: newH };
+  }
+  if (options.width) {
+    if (options.width <= meta.width) return options;
+    return { ...options, width: meta.width };
+  }
+  if (options.height) {
+    if (options.height <= meta.height) return options;
+    return { ...options, height: meta.height };
+  }
+  return options;
+};
 
 class StreamController{
   public static stream = (req: Request, res: Response<any>, next: NextFunction) => {
@@ -12,13 +50,13 @@ class StreamController{
     return res.send('Ready to Stream videos');
   }
 
-  public static images = async (req: Request, res: Response<any>, next: NextFunction) => {
+  public static images = async (req: any, res: Response<any>, next: NextFunction) => {
     const img = req.params.img;
     const basePath = './uploads/images/';
     const { h, w, fit } = req.query;
     console.log('img', img, h, w, fit, typeof(h), isNaN(h), isNaN(300));
     if (!fs.existsSync(basePath + img)) return res.send('File not found');
-    const options: any = {};
+    let options: any = {};
     let filename = img;
     if (w && !isNaN(w)) {
       options.width = Number(w);
@@ -34,6 +72,8 @@ class StreamController{
     }
 
     if (!fs.existsSync(basePath + filename)) {
+      options = await optimize(options, path.join(__dirname, `../.${basePath + img}`));
+      console.log(options);
       await sharp(basePath + img)
         .resize(options)
         .withMetadata()
