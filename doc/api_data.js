c0d2e74842d4e9ed0fe232f5161df10098876f81
Tidjani <mty.tidjani@gmail.com>
2020-05-29 23:47:37 +0100

    Apidoc config

---
+++ doc/api_data.js
@@ -0,0 +1,212 @@
+define({ "api": [
+  {
+    "type": "",
+    "url": "{class}",
+    "title": "",
+    "version": "0.0.0",
+    "filename": "src/controllers/streamController.ts",
+    "group": "/home/lenovo/Documents/PERSONAL/file-api-service/src/controllers/streamController.ts",
+    "groupTitle": "/home/lenovo/Documents/PERSONAL/file-api-service/src/controllers/streamController.ts",
+    "name": "Class"
+  },
+  {
+    "type": "method",
+    "url": "/vid/:vid",
+    "title": "Video_stream",
+    "name": "file-service-api",
+    "group": "Stream",
+    "version": "1.0.0",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Request",
+            "optional": false,
+            "field": "req",
+            "description": "<p>'Express request object'</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Response",
+            "optional": false,
+            "field": "res",
+            "description": "<p>'Express response object'</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "NextFunction",
+            "optional": false,
+            "field": "next",
+            "description": "<p>'Express next Function'</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "200": [
+          {
+            "group": "200",
+            "type": "file",
+            "optional": false,
+            "field": "file_requested",
+            "description": "<p>'video file'</p>"
+          }
+        ]
+      }
+    },
+    "filename": "src/controllers/streamController.ts",
+    "groupTitle": "Stream"
+  },
+  {
+    "type": "method",
+    "url": "/vid/:vid",
+    "title": "image_stream",
+    "name": "file-service-api",
+    "group": "Stream",
+    "version": "1.0.0",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Request",
+            "optional": false,
+            "field": "req",
+            "description": "<p>'Express request object'</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Response",
+            "optional": false,
+            "field": "res",
+            "description": "<p>'Express response object'</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "NextFunction",
+            "optional": false,
+            "field": "next",
+            "description": "<p>'Express next Function'</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "200": [
+          {
+            "group": "200",
+            "type": "file",
+            "optional": false,
+            "field": "file_requested",
+            "description": "<p>'image file'</p>"
+          }
+        ]
+      }
+    },
+    "filename": "src/controllers/streamController.ts",
+    "groupTitle": "Stream"
+  },
+  {
+    "type": "method",
+    "url": "/doc/:doc",
+    "title": "Video_stream",
+    "name": "file-service-api",
+    "group": "Stream",
+    "version": "1.0.0",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Request",
+            "optional": false,
+            "field": "req",
+            "description": "<p>'Express request object'</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Response",
+            "optional": false,
+            "field": "res",
+            "description": "<p>'Express response object'</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "NextFunction",
+            "optional": false,
+            "field": "next",
+            "description": "<p>'Express next Function'</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "200": [
+          {
+            "group": "200",
+            "type": "file",
+            "optional": false,
+            "field": "file_requested",
+            "description": "<p>'document file'</p>"
+          }
+        ]
+      }
+    },
+    "filename": "src/controllers/streamController.ts",
+    "groupTitle": "Stream"
+  },
+  {
+    "type": "method",
+    "url": "/vid/:vid",
+    "title": "Video_stream",
+    "name": "file-service-api",
+    "group": "Stream",
+    "version": "1.0.0",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Request",
+            "optional": false,
+            "field": "req",
+            "description": "<p>'Express request object'</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Response",
+            "optional": false,
+            "field": "res",
+            "description": "<p>'Express response object'</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "NextFunction",
+            "optional": false,
+            "field": "next",
+            "description": "<p>'Express next Function'</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "200": [
+          {
+            "group": "200",
+            "type": "file",
+            "optional": false,
+            "field": "file_requested",
+            "description": "<p>'video file'</p>"
+          }
+        ]
+      }
+    },
+    "filename": "src/controllers/streamController.ts",
+    "groupTitle": "Stream"
+  }
+] });
